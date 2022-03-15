e682347bb7b9e56f3be6438f40326523109df16b
Matheus Moreira <matheusmoreira1324@hotmail.com>
2022-03-15 14:53:19 -0300

    js-modules

---
+++ script.js
@@ -0,0 +1,15 @@
+import initScrollSuave from './modules/scrollSuave.js';
+import initTabNav from './modules/tabNav.js';
+import initAccordion from './modules/accordion.js';
+import initAnimacaoScroll from './modules/animacaoScroll.js';
+import initModal from './modules/modal.js'
+import initTolltip from './modules/tooltip.js'
+import initDropownMenu from './modules/dropdownmenu.js'
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTolltip();
+initDropownMenu();
